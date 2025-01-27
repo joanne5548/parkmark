be55e7655eefe6f7697044d38f20868de619d7fe
joanne <jkim13@ufl.edu>
2025-01-26 20:55:51 -0500

    get ready to deploy frontned on netlify

--- server/sql_queries/schema.sql
+++ server/sql_queries/schema.sql
@@ -6,6 +6,13 @@ CREATE TABLE UserData(
     created_at timestamptz DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Postgres gen_random_uuid produces version 4 UUID
+CREATE TABLE NationalPark (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255),
+    park_info JSONB NOT NULL
+);
+
 CREATE TABLE Review (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_sub_id VARCHAR(255) REFERENCES UserData(sub_id)
@@ -14,7 +21,7 @@ CREATE TABLE Review (
     park_id UUID REFERENCES NationalPark(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    rating INTEGER CHECK (rating >= 0 AND rating <= 5),
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
     content VARCHAR(2048),
     img_url_list JSONB DEFAULT NULL,
     created_at timestamptz DEFAULT CURRENT_TIMESTAMP
@@ -28,11 +35,4 @@ CREATE TABLE ThumbsUpList (
     review_id UUID REFERENCES Review(id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
-);
-
--- Postgres gen_random_uuid produces version 4 UUID
-CREATE TABLE NationalPark (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255),
-    park_info JSONB NOT NULL
 );
