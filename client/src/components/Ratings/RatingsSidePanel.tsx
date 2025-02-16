224a60b8a6dbd7d96d71683f4a27c2fef8d23c56
joanne <jkim13@ufl.edu>
2025-02-16 17:19:25 -0500

    moved reviews in panel into a tab

--- client/src/components/Ratings/RatingsSidePanel.tsx
+++ client/src/components/Ratings/RatingsSidePanel.tsx
@@ -6,6 +6,7 @@ import { MdAdd } from "react-icons/md";
 import RatingsCardList from "./Display/RatingsCardList";
 import RatingCardForm from "./Form/RatingCardForm";
 import { FiMinus } from "react-icons/fi";
+import ProfileTabs from "../ProfilePage/ProfileTabs";
 
 const RatingsSidePanel = () => {
     const setSelectedPark = useSetAtom(selectedParkAtom);
@@ -22,7 +23,7 @@ const RatingsSidePanel = () => {
     return (
         <div
             className="absolute left-2 right-2 bottom-2 h-[55%] sm:right-2 sm:left-auto sm:bottom-auto
-                sm:w-1/2 sm:max-w-[34rem] sm:h-[97%] pr-2 py-6 pl-5 sm:py-6 sm:pl-6
+                sm:w-1/2 sm:max-w-[34rem] sm:h-[97%] pb-6
                 flex flex-col z-[1000] bg-white rounded-lg border-slate-400 shadow-lg shadow-slate-400"
         >
             <button
@@ -31,24 +32,44 @@ const RatingsSidePanel = () => {
             >
                 <IoClose className="text-slate-500 size-5" />
             </button>
-            <div className="flex flex-col overflow-y-auto pr-4 h-full">
-                <div className="relative h-[50px] font-medium text-xl sm:text-2xl border-b-2 py-2">
-                    Reviews
-                    <div className="absolute right-0 bottom-1">
-                        <button
-                            onClick={handleNewReviewButtonOnClick}
-                            className="bg-amber-900 hover:bg-amber-800 rounded-md p-0.5"
-                        >
-                            {creatingNewReview ? (
-                                <FiMinus className="text-amber-100" />
-                            ) : (
-                                <MdAdd className="text-amber-100" />
-                            )}
-                        </button>
-                    </div>
-                </div>
-
-                {creatingNewReview ? <RatingCardForm resetCreatingNewReview={() => setCreatingNewReview(false)} /> : <RatingsCardList />}
+            <div className="flex flex-col h-full pt-1">
+                <ProfileTabs
+                    tabs={[
+                        {
+                            title: "Info",
+                            component: <div className="px-5">yo yoyoyoyoyoooy
+                            yo yo ma
+                            </div>,
+                        },
+                        {
+                            title: "Reviews",
+                            component: (
+                                <div className="relative h-full px-5 overflow-y-auto">
+                                    <button
+                                        onClick={handleNewReviewButtonOnClick}
+                                        className="absolute right-5 top-3 sm:top-2.5 text-xl sm:text-2xl bg-amber-900 hover:bg-amber-800 rounded-md p-0.5"
+                                    >
+                                        {creatingNewReview ? (
+                                            <FiMinus className="text-amber-100" />
+                                        ) : (
+                                            // <div className="text-amber-100 px-1">Create</div>
+                                            <MdAdd className="text-amber-100" />
+                                        )}
+                                    </button>
+                                    {creatingNewReview ? (
+                                        <RatingCardForm
+                                            resetCreatingNewReview={() =>
+                                                setCreatingNewReview(false)
+                                            }
+                                        />
+                                    ) : (
+                                        <RatingsCardList />
+                                    )}
+                                </div>
+                            ),
+                        },
+                    ]}
+                />
             </div>
         </div>
     );
--- client/src/lib/interfaces.ts
+++ client/src/lib/interfaces.ts
@@ -74,6 +74,6 @@ export interface ParkInfoJson {
         states: string;
         nps_url: string;
         description?: string;
-        img_urls?: string[];
+        img_url?: string;
     };
 }
